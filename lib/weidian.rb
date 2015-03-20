dd134ef7a3107b88a73623fc607db2ba6b75830e
YanyingWang <yanyingwangwang@gmail.com>
2015-03-20 21:43:57 +0800

    code indent

--- lib/weidian.rb
+++ lib/weidian.rb
@@ -4,23 +4,24 @@ require 'json'
 
 module Weidian
   API = { endpoint: 'https://api.vdian.com',
-                   version: '1.0',
-                   format: 'json' }
+          version: '1.0',
+          format: 'json' }
+
   HOWTO_CONFIG = <<-EOF
-    Configure #{self.name} fist like this:
-
   #{self.name}.config do
-      @app_key = '1111111'             # your app_key
-      @app_secret = '1111111111111111' # your secret
-    end
+    @app_key = '1111111'             # your app_key
+    @app_secret = '1111111111111111' # your secret
+  end
 
-    or:
+  or:
 
   #{self.name}.app_key = '111111'          # your app_key
   #{self.name}.app_secret = '1111111111'   # your secret
   EOF
 
+
   class << self
+
     alias_method :config, :instance_eval
     attr_accessor :app_key, :app_secret
 
@@ -28,24 +29,26 @@ module Weidian
       (@access_token && !access_token_expired?) ? @access_token : get_access_token_from_api
     end
 
-    # Gost Method here...
+
+    private
+
     def method_missing(method, **param)
       public = { method: method.to_s.gsub('_', '.'),
                  access_token: access_token,
                  version: API[:version],
                  format: API[:json] }
+
       url = API[:endpoint] + "/api?" + URI.encode_www_form(param: param.to_json, public: public.to_json)
+
       JSON.parse(eat url, :openssl_verify_mode => 'none')
     end
-    #def respond_to?(method)
 
-    private
     def get_access_token_from_api
       raise HOWTO_CONFIG unless @app_key or @app_secret
 
       url = API[:endpoint] + "/token?" + URI.encode_www_form(grant_type: "client_credential", 
-                                                           appkey: @app_key, 
-                                                           secret: @app_secret)
+                                                             appkey: @app_key,
+                                                             secret: @app_secret)
 
       result = JSON.parse(eat url, :openssl_verify_mode => 'none').fetch('result', {})
 
